cc5c709ec97654af7d8e620920aa00704bafd7ba
Ivan Ananchikov <ivanananchikov@gmail.com>
2018-08-30 03:38:10 +0300

    успешно передал бинарный файл 420 байт

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -30,9 +30,54 @@ void MainWindow::mySlot()
 
 void MainWindow::myReadSlot()
 {
-    //тот кто прислал данные, в нашем случае my_socket
-    /*auto*/ my_socket = dynamic_cast<QTcpSocket*>(sender());
-    ui->textEdit->append(QString(my_socket->readAll())+'\n');//read для файла
+//=============================================================
+    QByteArray data;
+    // read ...
+   /*auto*/ my_socket = dynamic_cast<QTcpSocket*>(sender());
+    //ui->textEdit->append(QString(my_socket->readAll())+'\n');//read для файла
+    data = my_socket->readAll();
+    QFile file("book2.bin"); // C:/path/myimage.jpg  change path
+    file.open(QIODevice::WriteOnly);
+    file.write(data);
+    file.close();
+//=============================================================
+//    //тот кто прислал данные, в нашем случае my_socket
+//    /*auto*/ my_socket = dynamic_cast<QTcpSocket*>(sender());
+//    ui->textEdit->append(QString(my_socket->readAll())+'\n');//read для файла
+//=============================================================
+
+
+
+//    QDataStream in(this);
+//        in.setVersion(10);
+//        if (nextBlockSize == 0) {
+//            if (bytesAvailable() < sizeof(quint16))
+//                return;
+//            in >> nextBlockSize;
+//        }
+//        if (bytesAvailable() < nextBlockSize)
+//            return;
+
+//        quint8 requestType;
+//        QString targetFile;
+//        targetFile = "book2.bin";
+
+//        in >> requestType;
+//        if (requestType == 'S') {
+//            in >> targetFile;
+//            generateRandomTrip(targetFile);
+//            QFile target(targetFile);
+//            if(target.open(QIODevice::WriteOnly)){
+//                char block[1024];
+
+//    //самое основное - пытаюсь считать этот файл из сетевого потока
+
+//                while(!in.atEnd()) {
+//                    quint64 tofile = in.readRawData(block,sizeof(block));
+
+//                    target.write(block,tofile);
+//=============================================================
+
 
 }
 
@@ -86,20 +131,72 @@ void MainWindow::on_send_on_FTP_clicked()
 
 void MainWindow::on_sendFile_clicked()
 {
-    QFile file("book.bin");
-    if(file.open(QIODevice::WriteOnly))
-    {
-        ui->textEdit->append("File book.bin is opened");
-        QDataStream stream(&file);
-        stream. setVersion(QDataStream::Qt_4_2);
-        stream << QImage("result.png");
-        if(stream.status() != QDataStream::Ok)
-        {
-            qDebug() << "Ошибка записи";
-            ui->textEdit->append("Record error!");
-        }
-    }
-    file.close();
+    QFile file("book.bin");//c:/mypic.jpg
+    file.open(QIODevice::ReadOnly);
+    QByteArray mydata=file.readAll();
+    my_socket->write(mydata);
+//=============================================================
+//    char block[1024];
+//    QFile fileopen("book.bin");
+//    if (fileopen.open(QFile::ReadWrite)) {
+//        QByteArray arrayBlock;
+//        QDataStream vivod(&arrayBlock, QIODevice::WriteOnly);
+//        vivod.setVersion(10);
+//        vivod << quint16(0) << quint8('S') << "book2.bin";
+
+//        fileopen.seek(0);
+//        while(!fileopen.atEnd()) {
+//             quint64 in;
+//            in = fileopen.read(block, sizeof(block));
+//            vivod.writeRawData(block,in);
+//            }
+//        vivod.device()->seek(0);
+//        vivod << quint16(arrayBlock.size() - sizeof(quint16));
+//        //записываем данные в сокет
+//        my_socket->write(arrayBlock);
+//        }
+//    ui->textEdit->append("Sending File...");
+//=============================================================
+
+
+//    unsigned int blockSize = 1024;
+//    QDataStream stream(my_socket);
+//    stream.setVersion(QDataStream::Qt_4_2);
+//    QFile file("book.bin");
+//    file.open(QIODevice::WriteOnly);
+//    if (blockSize == 0) {
+//        if (my_socket->bytesAvailable() < (int)sizeof(quint16))
+//            return;
+//        stream >> blockSize;
+//    }
+//    if (my_socket->bytesAvailable() < blockSize)
+//        return;
+//    char a[1024];
+//    while(stream.atEnd()!=true){
+//        int nBlockSize=stream.device()->read(a,sizeof(a));
+//        file.write(a, nBlockSize);
+//    }
+//    file.close();
+//=============================================================
+
+
+
+    //        QFile file("result2.png");
+    //        if(file.open(QIODevice::WriteOnly))
+    //        {
+    //            ui->textEdit->append("File result2.png is opened");
+    //            QDataStream stream(&file);
+    //            stream. setVersion(QDataStream::Qt_4_2);
+    //            stream << QImage("result.png");
+    //            if(stream.status() != QDataStream::Ok)
+    //            {
+    //                qDebug() << "Ошибка записи";
+    //                ui->textEdit->append("Record error!");
+    //            }
+    //        }
+    //        file.close();
+//=============================================================
+
 
 //    FileData fd;
 //    fd.FileSize = 1024;
